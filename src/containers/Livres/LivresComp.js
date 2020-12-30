582ce737bacacec479ff8783a1968237a38716ba
Seifo <seifallah27@gmail.com>
2020-12-30 11:13:17 +0100

    List/Livre_with_State/StateFull/Livres

--- src/containers/Livres/LivresComp.js
+++ src/containers/Livres/LivresComp.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {Component}  from 'react';
 import Botton from  "../../components/Bouton/BottonComp";
 
-const LivCompt = (props) => {
 
-     return ( 
 
-          <>
+class LivCompt extends Component {
+
+     state = {
+          livres : [
+               {id:1, titre:"L'algorithmique selon H2PROG", auteur:"Matthieu GASTON", nbPages:200},
+               {id:2, titre:"La France du 19ème", auteur: "Albert PATRICK", nbPages: 500},
+               {id:3, titre:"Le monde des animaux", auteur: "Marc MERLIN", nbPages: 250},
+               {id:4, titre:"Le Virus d'Asie", auteur: "Tya MILO", nbPages: 120},
+  
+          ]
+      }
+
+      render(){
+            return( 
+
+               <>
                <table className= "table text-center">
-                    <thead>
-                         <tr className="table-dark">
+                    <thead className="thead-dark" >
+                         <tr>
                               <th>Titre</th>
                               <th>Auteur</th>
                               <th>Nombre de pages</th>
@@ -16,18 +29,26 @@ const LivCompt = (props) => {
                          </tr>
                     </thead>
                     <tbody>
-                         <tr>
-                              <td>titre 1</td>
-                              <td>Auteur 1</td>
-                              <td>222</td>
-                              <td><Botton typeBtn ="btn-warning" clic = {()=> alert(" l'effet la fonction de clic modifier")}>Modifier</Botton></td>
-                              <td><Botton typeBtn ="btn-danger" clic = {()=> alert(" l'effet la fonction de clic supprimer")}>Supprimer</Botton></td>
-                         </tr>
+                         {
+                              this.state.livres.map(livre => {
+                                   return(
+                                        <tr>
+                                             <td>{livre.titre}</td>
+                                             <td>{livre.auteur}</td>
+                                             <td>{livre.nbPages}</td>
+                                             <td><Botton typeBtn ="btn-warning" clic = {()=> alert(" l'effet la fonction de clic modifier")}>Modifier</Botton></td>
+                                             <td><Botton typeBtn ="btn-danger" clic = {()=> alert(" l'effet la fonction de clic supprimer")}>Supprimer</Botton></td>
+                                        </tr>
+                                   );
+                              })
+                         }
+
                     </tbody>
                </table>
           </>
 
-      );
- } 
+             );
+      }
+}
 
 export default LivCompt;
