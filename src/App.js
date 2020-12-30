070a9b9ca94949776db2e6800eaa39783fa8d55a
Seifo <seifallah27@gmail.com>
2020-12-30 10:47:30 +0100

    add/Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import React from "react";
 function App() {
   return (
     <>
+       <div className="alert alert-success" role="alert">
+        Bootstrap est opérationnel
+      </div>
       <h1>Titre</h1>
       <p>Livres</p>
       <button type="submit"> Ajouter </button>
+
       
     </>
     
