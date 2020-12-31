7293f1343e440dbb1ed074a9eef6153a5d911399
Seifo <seifallah27@gmail.com>
2020-12-31 09:16:18 +0100

    modif/function-To-class

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React, {Component}  from 'react';
 import TitreH1 from "./components/Titres/TitreH1";
 import Botton from  "./components/Bouton/BottonComp"
 import LivresComp from "./containers/Livres/LivresComp"
 
-function App() {
-  return (
-    <>
-       
-      <TitreH1>Page Listant les livres</TitreH1>
-      <LivresComp> </LivresComp>
-      <Botton typeBtn ="btn-success" sizeBtn="w-100" clic = {()=> alert(" l'effet la fonction de clic du btn ajoute")}>Ajouter</Botton>
 
-      
-    </>
-    
-  );
+class App extends Component {
+   render(){
+      return( 
+        <>
+          <TitreH1>Page Listant les livres</TitreH1>
+          <LivresComp> </LivresComp>
+          <Botton typeBtn ="btn-success" sizeBtn="w-100" clic = {()=> alert(" l'effet la fonction de clic du btn ajoute")}>Ajouter</Botton>
+        </>
+
+       );
+   }
 }
 
-export default App;
+ export default App;
